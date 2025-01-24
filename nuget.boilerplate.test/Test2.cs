f902f6d1946ed66c8809807fede0341b7e0f5f64
Luis Hernández Piney <luis.piney@gmail.com>
2025-01-24 22:23:30 +0100

    Add new test class

---
+++ nuget.boilerplate.test/Test2.cs
@@ -0,0 +1,14 @@
+﻿using nuget_boiler_plate;
+
+namespace nuget.boilerplate.test;
+
+public class Test2
+{
+    [Fact]
+    public void Test1()
+    {
+        var class1 = new Class1();
+
+        Assert.NotNull(class1);
+    }
+}
