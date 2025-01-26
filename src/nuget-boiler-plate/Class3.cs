a670fb0600563156bbc9be29122708e745dabd30
Luis Hernández Piney <luis.piney@gmail.com>
2025-01-26 18:38:17 +0100

    Update Class3.cs

--- src/nuget-boiler-plate/Class3.cs
+++ src/nuget-boiler-plate/Class3.cs
@@ -1,8 +1,31 @@
 using System;
+using System.Collections.Generic;
 
 namespace nuget_boiler_plate;
 
 public class Class3
 {
+    public string Name { get; set; }
+    public int Age { get; set; }
 
+    public Class3(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+    
+    public int GetAge()
+    {
+        return Age;
+    }
+    
+    public override string ToString()
+    {
+        return $"Name: {Name}, Age: {Age}";
+    }
 }
